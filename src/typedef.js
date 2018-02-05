dcfc059edb2d4a954037aae9e915eeebb385fb11
homer0 <leo.apiwan@olapic.com>
2018-02-05 08:58:27 -0300

    Add typedefs

---
+++ src/typedef.js
@@ -0,0 +1,9 @@
+/**
+ * @external {Woopack}
+ * https://homer0.github.io/woopack/class/src/app/index.js~Woopack.html
+ */
+
+/**
+ * @external {Target}
+ * https://homer0.github.io/woopack/typedef/index.html#static-typedef-Target
+ */
