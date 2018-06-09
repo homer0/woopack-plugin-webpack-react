c9ed7c5a823d92bcaf841970084c1a7f3df2769a
homer0 <leo.apiwan@olapic.com>
2018-06-09 00:17:32 -0300

    Hide definition of types that are not visible on the docs

--- src/typedef.js
+++ src/typedef.js
@@ -6,9 +6,11 @@
 /**
  * @external {Target}
  * https://homer0.github.io/projext/typedef/index.html#static-typedef-Target
+ * @ignore
  */
 
 /**
  * @external {Targets}
  * https://homer0.github.io/projext/class/src/services/targets/targets.js~Targets.html
+ * @ignore
  */
